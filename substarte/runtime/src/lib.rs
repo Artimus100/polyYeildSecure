3d8895d980063631b1d82593083f40d8b417ba13
Rahul <rahulmishrapu22@gmail.com>
2024-07-10 08:39:21 +0530

    added_binaries

--- substarte/runtime/src/lib.rs
+++
@@ -1,7 +0,0 @@
-impl pallet_cross_chain::Config for Runtime {
-    type Event = Event;
-    type Currency = Balances;
-}
-
-// In the construct_runtime! macro
-CrossChain: pallet_cross_chain::{Pallet, Call, Storage, Event<T>},
